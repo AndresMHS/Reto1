b893d50a7e45fa4c77f54ededf7b479d9eeab695
Andres Haro <andres_h03@outlook.com>
2020-12-19 11:21:47 -0700

    V0.0.2

--- src/app/pages/new-student/new-student.page.ts
+++ src/app/pages/new-student/new-student.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormGroup, FormBuilder} from '@angular/forms'
+import { FormGroup, FormBuilder, Validators} from '@angular/forms'
 import { Estudiante } from '../../models/estudiante';
 import { EstudianteService } from '../../services/estudiante.service';
 
@@ -18,11 +18,11 @@ export class NewStudentPage implements OnInit {
 
   ngOnInit() {
     this.myForm = this.fb.group({
-      name: [""],
-      controlnumber: [""],
-      curp: [""],
-      age: [0],
-      active: [false]
+      name: ["",[Validators.required, Validators.minLength(3),Validators.maxLength(150)]],
+      controlnumber: ["",[Validators.required, Validators.minLength(10),Validators.maxLength(10)]],
+      curp: ["",[Validators.required, Validators.pattern('([A-Z]{4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[HM](AS|BC|BS|CC|CL|CM|CS|CH|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[A-Z]{3}[0-9]{2})')]],
+      age: [0,[Validators.required]],
+      active: [false,[Validators.required]]
     });
   }
 
